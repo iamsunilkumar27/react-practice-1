91670958837bb8febc213d39174aae00cb6026ce
sunil kumar <iamsunilkumarsk7@gmail.com>
2021-07-16 12:01:21 +0530

    ...

--- src/App.js
+++ src/App.js
@@ -1,25 +1,62 @@
-import logo from './logo.svg';
 import './App.css';
+import { Component } from 'react';
+
+class Box extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      color: this.props.color,
+      border:this.props.border,
+    }
+  }
+
+componentDidMount(){
+  console.log("mounted")
+}
+componentDidUpdate(){
+  console.log("updated");
+}
+componentWillUnmount(){
+  console.log("destroyed");
+}
+
+  render() {
+    return (
+      <div className="box" style={{ backgroundColor: this.state.color ,borderRadius:this.state.border }}>
+        <button onClick={() => {
+          const col= randomcolor();
+          const nstate = Object.assign({},this.state,{color:col})
+          this.setState(nstate);
+          this.setState({border: (this.state.border = this.state.border === '0%'? '50%': '0%')})
+        }}>{ this.state.border === '0%' ? 'circle' : 'box'}</button>
+      </div>
+    )
+  }
+}
+
+
+const colorValues = '0123456789ABCDEF';
+const colors = Array(10).fill(0).map(randomcolor)
+
+// console.log(colors);
+function randomcolor() {
+  let col = '#';
+  for (let i = 0; i < 6; i++) {
+    col = col + colorValues[Math.floor(Math.random() * colorValues.length)];
+  }
+  return col;
+
+}
+
 
 function App() {
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div style={{display:'flex'}}>
+      {colors.map(c => <Box key={c + Date.now()} color={c} border="0%" />)}
     </div>
   );
+
 }
 
 export default App;
